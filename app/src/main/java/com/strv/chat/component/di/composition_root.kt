dd3eaaf402364170c04d7936d26d5bfd21da30fa
ywett02 <ivet.jurcikova@gmail.com>
2019-09-27 19:37:30 +0200

    Move all instance creations to composition root

--- app/src/main/java/com/strv/chat/component/di/composition_root.kt
+++ app/src/main/java/com/strv/chat/component/di/composition_root.kt
@@ -1,13 +1,29 @@
 package com.strv.chat.component.di
 
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import com.strv.chat.component.business.MediaProviderImpl
 import com.strv.chat.component.business.MemberProviderImpl
+import com.strv.chat.library.cloudStorage.di.cloudStorageMediaClient
+import com.strv.chat.library.domain.client.ChatClient
+import com.strv.chat.library.domain.client.ConversationClient
+import com.strv.chat.library.domain.client.MediaClient
 import com.strv.chat.library.domain.provider.MediaProvider
 import com.strv.chat.library.domain.provider.MemberProvider
+import com.strv.chat.library.firestore.di.firestoreChatClient
+import com.strv.chat.library.firestore.di.firestoreConversationClient
 
 class CompositionRoot {
 
     fun mediaProvider(): MediaProvider = MediaProviderImpl()
 
     fun memberProvider(): MemberProvider = MemberProviderImpl()
+
+    fun chatClient(firebaseDb: FirebaseFirestore): ChatClient = firestoreChatClient(firebaseDb)
+
+    fun conversationClient(firebaseDb: FirebaseFirestore): ConversationClient =
+        firestoreConversationClient(firebaseDb)
+
+    fun mediaClient(firebaseStore: FirebaseStorage): MediaClient =
+        cloudStorageMediaClient(firebaseStore)
 }
