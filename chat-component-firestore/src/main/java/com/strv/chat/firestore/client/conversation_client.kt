79ad5d6a1b283fcf9e257df26459e58d3049986e
ywett02 <ivet.jurcikova@gmail.com>
2019-10-04 12:09:22 +0200

    Add a method that creates a new conversation

--- chat-component-firestore/src/main/java/com/strv/chat/firestore/client/conversation_client.kt
+++ chat-component-firestore/src/main/java/com/strv/chat/firestore/client/conversation_client.kt
@@ -2,9 +2,14 @@ package com.strv.chat.firestore.client
 
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
+import com.strv.chat.core.domain.Task
 import com.strv.chat.core.domain.client.ConversationClient
 import com.strv.chat.core.domain.map
+import com.strv.chat.core.domain.task
+import com.strv.chat.firestore.CONVERSATIONS_COLLECTION
+import com.strv.chat.firestore.SEEN_COLLECTION
 import com.strv.chat.firestore.entity.FirestoreConversationEntity
+import com.strv.chat.firestore.entity.FirestoreSeenEntity
 import com.strv.chat.firestore.firestoreConversations
 import com.strv.chat.firestore.listSource
 import com.strv.chat.firestore.model.creator.ConversationModelListConfiguration
@@ -14,8 +19,33 @@ class FirestoreConversationClient(
     val firebaseDb: FirebaseFirestore
 ) : ConversationClient {
 
-    override fun subscribeConversations(userId: String) =
-        firestoreListSource(firestoreConversations(firebaseDb, userId))
+    override fun createConversation(memberIds: List<String>): Task<String, Throwable> =
+        task {
+            val conversationDocument =
+                firebaseDb.collection(CONVERSATIONS_COLLECTION).document()
+
+            val seenCollection = conversationDocument.collection(SEEN_COLLECTION)
+
+            firebaseDb.batch().run {
+                set(
+                    conversationDocument, FirestoreConversationEntity(members = memberIds).toMap()
+                )
+
+                for (id in memberIds) {
+                    set(seenCollection.document(id), FirestoreSeenEntity().toMap(false))
+                }
+
+                commit()
+            }.addOnSuccessListener {
+                invokeSuccess(conversationDocument.id)
+
+            }.addOnFailureListener { error ->
+                invokeError(error)
+            }
+        }
+
+    override fun subscribeConversations(memberId: String) =
+        firestoreListSource(firestoreConversations(firebaseDb, memberId))
             .subscribe()
             .map { conversation ->
                 ConversationModelListCreator.create(ConversationModelListConfiguration(conversation))
