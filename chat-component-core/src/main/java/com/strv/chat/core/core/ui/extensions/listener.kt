f643b5c2bd07b2d9e099f05b3435c33fd16b20ee
ywett02 <ivet.jurcikova@gmail.com>
2019-11-09 15:56:09 +0100

    Make OnClickAction public

--- chat-component-core/src/main/java/com/strv/chat/core/core/ui/extensions/listener.kt
+++ chat-component-core/src/main/java/com/strv/chat/core/core/ui/extensions/listener.kt
@@ -2,4 +2,4 @@ package com.strv.chat.core.core.ui.extensions
 
 internal const val ON_CLICK_ACTION = "onClickAction"
 
-internal typealias OnClickAction<T> = (T) -> Unit
+typealias OnClickAction<T> = (T) -> Unit
