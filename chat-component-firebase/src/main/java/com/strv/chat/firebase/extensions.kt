65b826bca7bc9d83c604f19954c009a805eaffc1
ywett02 <ivet.jurcikova@gmail.com>
2019-11-09 22:51:52 +0100

    Remove firebase folder

--- chat-component-firebase/src/main/java/com/strv/chat/firebase/extensions.kt
+++
@@ -1,8 +0,0 @@
-package com.strv.chat.firebase
-
-import com.google.firebase.database.DatabaseReference
-import com.strv.chat.core.data.entity.SourceEntity
-import com.strv.chat.core.data.source.ListSource
-
-inline fun <reified T: SourceEntity> DatabaseReference.listSource(): ListSource<T> =
-    FirebaseListSource(this, T::class.java)
